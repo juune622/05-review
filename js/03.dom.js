b45aafaf114758384330610a19c2a05966b100e2
juune622 <juune622@naver.com>
2020-12-09 11:14:26 +0900

    ...

---
+++ js/03.dom.js
@@ -0,0 +1,38 @@
+/* 전역변수 */
+var $listWrap = $('.list-wrap');
+var tag = '<h2>추가내용</h2>'
+
+/* 이벤트등록 */
+$('#btAppend').click(onAppend);
+$('#btPrepend').click(onPrepend);
+$('#btEmpty').click(onEmpty);
+$('#btRemove').click(onRemove);
+$('#btHtml').click(onHtml);
+$('#btText').click(onText);
+$('#btReset').click(onReset);
+
+/* 이벤트콜백 */
+function onAppend(){
+    $listWrap.append(tag);
+    $(tag).appendTo($listWrap)
+}
+
+function onPrepend() {
+    $listWrap.prepend(tag);
+    $(tag).prependTo($listWrap)
+    
+}function onEmpty() {
+    $listWrap.empty();
+   
+}function onRemove() {
+    $listWrap.remove();
+}function onHtml() {
+    $listWrap.html('<h1>Hello</h1>');
+}
+function onText() {
+    $listWrap.text('<h1>Hello</h1>');
+}
+function onReset() {
+    $('.container').html('<div class="list-wrap text-center"><div>===== 기준 =====</div></div>');
+    $listWrap= $('.list-wrap')
+}
